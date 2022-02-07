c72b905b73cc14638d5c578e8d5a2f725dbb635e
durmazmerve <95907796+durmazmerve@users.noreply.github.com>
2022-02-07 18:00:41 +0300

    Add files via upload
    124 bin TL anaparanın %18 yıllık faiz ile mevduat hesabına yatırıldığında birinci yıl ve ikinci yıl sonunda ulaşacağı tutarları bulan Java kodu;

---
+++ FaizHesaplama.java
@@ -0,0 +1,13 @@
+ 
+package X;
+    public class FaizHesaplama { 
+    public static void main(String[] args) { 
+        double anaPara=124_000; 
+        anaPara = 0.18*anaPara + anaPara; 
+        System.out.println("Birinci yıl sonunda ana para = "+anaPara); 
+        anaPara = 0.18*anaPara + anaPara; 
+        System.out.println("İkinci yıl sonunda ana para = "+anaPara);    
+      } 
+}
+    
+ 
